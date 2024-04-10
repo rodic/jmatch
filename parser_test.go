2cd2e86b66c8f6dc7bacae2873b719f965d83673
Aleksandar Rodić <aleks.rodic@pm.me>
2024-04-10 19:31:18 +0200

    Improving tests

--- parser_test.go
+++ parser_test.go
@@ -114,7 +114,6 @@ func TestSuccessParse(t *testing.T) {
 				".[1]": Token{tokenType: String, Value: "2"},
 			},
 		},
-
 		{name: "['1', ['2']]",
 			tokens: []Token{
 				{tokenType: LeftBracket, Value: "["},
@@ -128,6 +127,29 @@ func TestSuccessParse(t *testing.T) {
 			expected: parsingResult{
 				".[0]":     Token{tokenType: String, Value: "1"},
 				".[1].[0]": Token{tokenType: String, Value: "2"},
+			},
+		},
+		{name: "[{}]",
+			tokens: []Token{
+				{tokenType: LeftBracket, Value: "["},
+				{tokenType: LeftBrace, Value: "{"},
+				{tokenType: RightBrace, Value: "}"},
+				{tokenType: RightBracket, Value: "]"},
+			},
+			expected: parsingResult{},
+		},
+		{name: "[{'a': 1}]",
+			tokens: []Token{
+				{tokenType: LeftBracket, Value: "["},
+				{tokenType: LeftBrace, Value: "{"},
+				{tokenType: String, Value: "a"},
+				{tokenType: Colon, Value: ":"},
+				{tokenType: Number, Value: "1"},
+				{tokenType: RightBrace, Value: "}"},
+				{tokenType: RightBracket, Value: "]"},
+			},
+			expected: parsingResult{
+				".[0].a": Token{tokenType: Number, Value: "1"},
 			},
 		},
 		{name: "{'a': [[[[[[1]]]]]]}",
@@ -169,7 +191,6 @@ func TestSuccessParse(t *testing.T) {
 				".a.[1]": Token{tokenType: String, Value: "2"},
 			},
 		},
-
 		{name: "{'a': [[[[[[1]]]]]]}",
 			tokens: []Token{
 				{tokenType: LeftBrace, Value: "{"},
@@ -194,7 +215,6 @@ func TestSuccessParse(t *testing.T) {
 				".a.[0].[0].[0].[0].[0].[0]": Token{tokenType: String, Value: "1"},
 			},
 		},
-
 		{name: "{'a': ['1', ['2', '3']]}",
 			tokens: []Token{
 				{tokenType: LeftBrace, Value: "{"},
@@ -217,7 +237,6 @@ func TestSuccessParse(t *testing.T) {
 				".a.[1].[1]": Token{tokenType: String, Value: "3"},
 			},
 		},
-
 		{name: "{'a': ['1', {'b': ['2', {'c': 3}]}]}",
 			tokens: []Token{
 				{tokenType: LeftBrace, Value: "{"},
@@ -248,7 +267,6 @@ func TestSuccessParse(t *testing.T) {
 				".a.[1].b.[1].c": Token{tokenType: String, Value: "3"},
 			},
 		},
-
 		{name: "{'a': ['1', '2'], 'b': '3', 'c': ['4']}",
 			tokens: []Token{
 				{tokenType: LeftBrace, Value: "{"},
@@ -278,7 +296,6 @@ func TestSuccessParse(t *testing.T) {
 				".c.[0]": Token{tokenType: String, Value: "4"},
 			},
 		},
-
 		{name: "{'s': {'t': [[1], -2.0, '3', true, {'x': false}]}}",
 			tokens: []Token{
 				{tokenType: LeftBrace, Value: "{"},
@@ -450,7 +467,6 @@ func TestFailParse(t *testing.T) {
 				{tokenType: RightBrace, Value: "}", line: 1, column: 7},
 			},
 			expected: "invalid JSON. unexpected token 2 found at line 1 column 6"},
-
 		{name: "{'a': 'b': 1}",
 			tokens: []Token{
 				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
@@ -547,6 +563,81 @@ func TestFailParse(t *testing.T) {
 				{tokenType: RightBrace, Value: "}", line: 1, column: 2},
 			},
 			expected: "invalid JSON. unexpected token } found at line 1 column 2"},
+		{name: "{[}",
+			tokens: []Token{
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 2},
+				{tokenType: RightBrace, Value: "}", line: 1, column: 3},
+			},
+			expected: "invalid JSON. unexpected token [ found at line 1 column 2"},
+		{name: "{'a': [}",
+			tokens: []Token{
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
+				{tokenType: String, Value: "a", line: 1, column: 2},
+				{tokenType: Colon, Value: ":", line: 1, column: 3},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 4},
+				{tokenType: RightBrace, Value: "}", line: 1, column: 5},
+			},
+			expected: "invalid JSON. unexpected token } found at line 1 column 5"},
+		{name: "{'a': [,",
+			tokens: []Token{
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
+				{tokenType: String, Value: "a", line: 1, column: 2},
+				{tokenType: Colon, Value: ":", line: 1, column: 3},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 4},
+				{tokenType: Comma, Value: ",", line: 1, column: 5},
+			},
+			expected: "invalid JSON. unexpected token , found at line 1 column 5"},
+		{name: "{'a': [,",
+			tokens: []Token{
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
+				{tokenType: String, Value: "a", line: 1, column: 2},
+				{tokenType: Colon, Value: ":", line: 1, column: 3},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 4},
+				{tokenType: Comma, Value: ",", line: 1, column: 5},
+			},
+			expected: "invalid JSON. unexpected token , found at line 1 column 5"},
+		{name: "{'a': [{},",
+			tokens: []Token{
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
+				{tokenType: String, Value: "a", line: 1, column: 2},
+				{tokenType: Colon, Value: ":", line: 1, column: 3},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 4},
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 5},
+				{tokenType: RightBrace, Value: "}", line: 1, column: 6},
+			},
+			expected: "invalid JSON. Unexpected end of JSON input"},
+		{name: "{'s': {'t': [[1], -2.0, '3', true, {'x': false,}]}}",
+			tokens: []Token{
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 1},
+				{tokenType: String, Value: "s", line: 1, column: 2},
+				{tokenType: Colon, Value: ":", line: 1, column: 3},
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 4},
+				{tokenType: String, Value: "t", line: 1, column: 5},
+				{tokenType: Colon, Value: ":", line: 1, column: 6},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 7},
+				{tokenType: LeftBracket, Value: "[", line: 1, column: 8},
+				{tokenType: Number, Value: "1", line: 1, column: 9},
+				{tokenType: RightBracket, Value: "]", line: 1, column: 10},
+				{tokenType: Comma, Value: ",", line: 1, column: 11},
+				{tokenType: Number, Value: "-2.0", line: 1, column: 12},
+				{tokenType: Comma, Value: ",", line: 1, column: 13},
+				{tokenType: String, Value: "3", line: 1, column: 14},
+				{tokenType: Comma, Value: ",", line: 1, column: 15},
+				{tokenType: Boolean, Value: "true", line: 1, column: 16},
+				{tokenType: Comma, Value: ",", line: 1, column: 17},
+				{tokenType: LeftBrace, Value: "{", line: 1, column: 18},
+				{tokenType: String, Value: "x", line: 1, column: 19},
+				{tokenType: Colon, Value: ":", line: 1, column: 20},
+				{tokenType: Boolean, Value: "false", line: 1, column: 21},
+				{tokenType: Comma, Value: ",", line: 1, column: 22},
+				{tokenType: RightBrace, Value: "}", line: 1, column: 23},
+				{tokenType: RightBracket, Value: "]", line: 1, column: 24},
+				{tokenType: RightBrace, Value: "}", line: 1, column: 25},
+				{tokenType: RightBrace, Value: "}", line: 1, column: 26},
+			},
+			expected: "invalid JSON. unexpected token } found at line 1 column 23",
+		},
 	}
 
 	for _, tc := range testCases {
