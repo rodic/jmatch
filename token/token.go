db47bc10adf428351dfbe654655e34d616e6a2f0
Aleksandar Rodić <aleks.rodic@pm.me>
2024-04-12 12:26:43 +0200

    Refactoring token

--- token/token.go
+++ token/token.go
@@ -2,117 +2,117 @@ package token
 
 import c "github.com/rodic/jmatch/common"
 
-type TokenType int
-
-type Token struct {
-	tokenType TokenType
-	Value     string
-	line      int
-	column    int
-}
+type tokenType int
 
 const (
-	LeftBrace TokenType = iota
-	RightBrace
-	LeftBracket
-	RightBracket
-	Comma
-	String
-	Number
-	Boolean
-	Null
-	Colon
+	leftBrace tokenType = iota
+	rightBrace
+	leftBracket
+	rightBracket
+	comma
+	str
+	number
+	boolean
+	null
+	colon
 )
 
-func (t Token) IsLeftBrace() bool {
-	return t.tokenType == LeftBrace
+type Token struct {
+	_type  tokenType
+	Value  string
+	line   int
+	column int
 }
 
-func (t Token) IsRightBrace() bool {
-	return t.tokenType == RightBrace
-}
-
-func (t Token) IsLeftBracket() bool {
-	return t.tokenType == LeftBracket
-}
-
-func (t Token) IsRightBracket() bool {
-	return t.tokenType == RightBracket
-}
-
-func (t Token) IsComma() bool {
-	return t.tokenType == Comma
-}
-
-func (t Token) IsString() bool {
-	return t.tokenType == String
-}
-
-func (t Token) IsNumber() bool {
-	return t.tokenType == Number
-}
-
-func (t Token) IsBoolean() bool {
-	return t.tokenType == Boolean
-}
-
-func (t Token) IsNull() bool {
-	return t.tokenType == Null
-}
-
-func (t Token) IsColon() bool {
-	return t.tokenType == Colon
-}
-
-func (t Token) AsUnexpectedTokenErr() c.UnexpectedTokenErr {
-	return c.UnexpectedTokenErr{Token: t.Value, Line: t.line, Column: t.column}
-}
-
-func NewToken(tokenType TokenType, value string, line int, column int) Token {
+func new(t tokenType, value string, line int, column int) Token {
 	return Token{
-		tokenType: tokenType,
-		Value:     value,
-		line:      line,
-		column:    column,
+		_type:  t,
+		Value:  value,
+		line:   line,
+		column: column,
 	}
 }
 
 func NewStringToken(value string, line int, column int) Token {
-	return Token{tokenType: String, Value: value, line: line, column: column}
+	return Token{_type: str, Value: value, line: line, column: column}
 }
 
 func NewNumberToken(value string, line int, column int) Token {
-	return NewToken(Number, value, line, column)
+	return new(number, value, line, column)
 }
 
 func NewBooleanToken(value string, line int, column int) Token {
-	return NewToken(Boolean, value, line, column)
+	return new(boolean, value, line, column)
 }
 
 func NewNullToken(line int, column int) Token {
-	return NewToken(Null, "null", line, column)
+	return new(null, "null", line, column)
 }
 
 func NewLeftBraceToken(line int, column int) Token {
-	return Token{tokenType: LeftBrace, Value: "{", line: line, column: column}
+	return Token{_type: leftBrace, Value: "{", line: line, column: column}
 }
 
 func NewRightBraceToken(line int, column int) Token {
-	return Token{tokenType: RightBrace, Value: "}", line: line, column: column}
+	return Token{_type: rightBrace, Value: "}", line: line, column: column}
 }
 
 func NewLeftBracketToken(line int, column int) Token {
-	return Token{tokenType: LeftBracket, Value: "[", line: line, column: column}
+	return Token{_type: leftBracket, Value: "[", line: line, column: column}
 }
 
 func NewRightBracketToken(line int, column int) Token {
-	return Token{tokenType: RightBracket, Value: "]", line: line, column: column}
+	return Token{_type: rightBracket, Value: "]", line: line, column: column}
 }
 
 func NewColonToken(line int, column int) Token {
-	return Token{tokenType: Colon, Value: ":", line: line, column: column}
+	return Token{_type: colon, Value: ":", line: line, column: column}
 }
 
 func NewCommaToken(line int, column int) Token {
-	return Token{tokenType: Comma, Value: ",", line: line, column: column}
+	return Token{_type: comma, Value: ",", line: line, column: column}
+}
+
+func (t Token) IsLeftBrace() bool {
+	return t._type == leftBrace
+}
+
+func (t Token) IsRightBrace() bool {
+	return t._type == rightBrace
+}
+
+func (t Token) IsLeftBracket() bool {
+	return t._type == leftBracket
+}
+
+func (t Token) IsRightBracket() bool {
+	return t._type == rightBracket
+}
+
+func (t Token) IsComma() bool {
+	return t._type == comma
+}
+
+func (t Token) IsString() bool {
+	return t._type == str
+}
+
+func (t Token) IsNumber() bool {
+	return t._type == number
+}
+
+func (t Token) IsBoolean() bool {
+	return t._type == boolean
+}
+
+func (t Token) IsNull() bool {
+	return t._type == null
+}
+
+func (t Token) IsColon() bool {
+	return t._type == colon
+}
+
+func (t Token) AsUnexpectedTokenErr() c.UnexpectedTokenErr {
+	return c.UnexpectedTokenErr{Token: t.Value, Line: t.line, Column: t.column}
 }
